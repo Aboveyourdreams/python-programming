aecf211dffedb1f03fa524c96d5c89872eaf5281
Aboveyourdreams <81636719+Aboveyourdreams@users.noreply.github.com>
2021-04-02 19:34:36 +0430

    Add files via upload

---
+++ multiplication table(for).py
@@ -0,0 +1,4 @@
+for a in range(10):
+    for b in range(10):
+        print((a+1)*(b+1),end='   ')
+    print('\r\n')
---
+++ multiplication table(while).py
@@ -0,0 +1,9 @@
+a=1
+b=1
+while a<11:
+    while b<11:
+        print(a*b,end='  ')
+        b=b+1
+    print('\r\n')
+    a=a+1
+    b=1
