989b3a00d98eb2dba0e3964837307f732d38d9ef
Del <81636719+Aboveyourdreams@users.noreply.github.com>
2021-04-03 12:40:18 +0430

    Add files via upload

---
+++ salary.py
@@ -0,0 +1,8 @@
+a=0
+salary=[1000000,985000,1200000,1150000,2000000,2300000,1750000,2000000,1500000,1900000,2500000,790000]
+for n in salary:
+    a=a+n
+print("salay is:"," ",a)
+b=len(salary)
+c=a/b
+print("avarage salary is:","    ",c)
